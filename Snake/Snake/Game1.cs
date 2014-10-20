b256796a55303d9903805f9802271e605ace4f06
Daviex <david.iuffri94@hotmail.it>
2014-10-20 19:02:40 +0200

    Added border to Window

--- Snake/Snake/Game1.cs
+++ Snake/Snake/Game1.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
@@ -19,24 +21,31 @@ namespace Snake
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        #region Dependencies
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
         Texture2D snakeTex;
+        Texture2D border;
 
         List<Vector2> snakeStruct;
-        Rectangle snakeBlocks;
-
-        TextWriter text = new StreamWriter("cordinates.txt");
 
         int cw = 10;
         int score = 0;
 
         string dir;
 
+        #endregion
+
+        #region Initial Functions
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferMultiSampling = true;
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
             Content.RootDirectory = "Content";
         }
 
@@ -50,10 +59,12 @@ namespace Snake
         {
             createSnake();
 
-
-
             base.Initialize();
         }
+
+        #endregion
+
+        #region Content
 
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
@@ -61,9 +72,10 @@ namespace Snake
         /// </summary>
         protected override void LoadContent()
         {
-            snakeTex = Content.Load<Texture2D>("Snake");
-
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            snakeTex = Content.Load<Texture2D>("Snake");
+            border = new Texture2D(GraphicsDevice, 1, 1);
+            border.SetData<Color>(new Color[] { Color.White });
         }
 
         /// <summary>
@@ -73,6 +85,10 @@ namespace Snake
         protected override void UnloadContent()
         {
         }
+
+        #endregion
+
+        #region Update
 
         /// <summary>
         /// Allows the game to run logic such as updating the world,
@@ -102,20 +118,23 @@ namespace Snake
                 headPos.Y++;
 
             snakeStruct[snakeStruct.Count - 1] = headPos;
-            text.WriteLine("HeadPos.X: " + headPos.X + "  HeadPos.Y: " + headPos.Y);
+            Console.WriteLine("HeadPos.X: " + headPos.X + "  HeadPos.Y: " + headPos.Y);
 
             for (int i = snakeStruct.Count - 2; i >= 0; i--)
             {
-                text.WriteLine("First Was: " + i + "° Body.X: " + snakeStruct[i].X + "  " + i + "° Body.Y: " + snakeStruct[i].Y);
+                Console.WriteLine("First Was: " + i + "° Body.X: " + snakeStruct[i].X + "  " + i + "° Body.Y: " + snakeStruct[i].Y);
 
                 snakeStruct[i] = new Vector2(snakeStruct[i].X + (headPos.X - snakeStruct[i].X) - 10, snakeStruct[i].Y + (headPos.Y - snakeStruct[i].Y) - 10);
 
-                text.WriteLine("Later Was: " + i + "° Body.X: " + snakeStruct[i].X + "  " + i + "° Body.Y: " + snakeStruct[i].Y);
-                text.Flush();
+                Console.WriteLine("Later Was: " + i + "° Body.X: " + snakeStruct[i].X + "  " + i + "° Body.Y: " + snakeStruct[i].Y);
             }
 
             base.Update(gameTime);
         }
+
+        #endregion
+
+        #region Draw
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -127,13 +146,59 @@ namespace Snake
 
             spriteBatch.Begin();
 
+            //Draw snake
             foreach (Vector2 body in snakeStruct)
                 spriteBatch.Draw(snakeTex, body, Color.White);
+
+            //Draw borders of the game
+            DrawLine(new Vector2(5, 5), new Vector2(795, 5), true); //Upper Border
+            DrawLine(new Vector2(5, 5), new Vector2(5, 595), false); //Left Border
+            DrawLine(new Vector2(795, 5), new Vector2(795, 597), false); //Right Border
+            DrawLine(new Vector2(5, 595), new Vector2(795, 595), true); //Bottom Border
 
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        protected void DrawLine(Vector2 StartPoint, Vector2 EndPoint, bool isHorizontal)
+        {
+            Vector2 edge = EndPoint - StartPoint;
+
+            if(isHorizontal)
+                spriteBatch.Draw(
+                    border, //Texture2D
+                    new Rectangle( //Declare new rectangle ( For collisions )
+                        (int)StartPoint.X, //X Start
+                        (int)StartPoint.Y, //Y Start
+                        (int)edge.Length(), //Width of line
+                        2), //Height of line
+                    null, //Source Rectangle 
+                    Color.Black, //Color of line
+                    0, //Rotation
+                    Vector2.Zero, //Origin
+                    SpriteEffects.None, //Sprite Effect
+                    0); //LayerDepth
+            else
+                spriteBatch.Draw(
+                    border, //Texture2D
+                    new Rectangle( //Declare new rectangle ( For collisions )
+                        (int)StartPoint.X, //X Start
+                        (int)StartPoint.Y, //Y Start
+                        2, //Width of line
+                        (int)edge.Length()), //Height of line
+                    null, //Source Rectangle 
+                    Color.Black, //Color of line
+                    0, //Rotation
+                    Vector2.Zero, //Origin
+                    SpriteEffects.None, //Sprite Effect
+                    0); //LayerDepth
+
+        }
+
+        #endregion
+
+        #region My Functions
 
         protected void createSnake()
         {
@@ -144,8 +209,10 @@ namespace Snake
 
             for (int i = 0; i < length; i++)
             {
-                snakeStruct.Add(new Vector2(i * cw + 2, 2));
+                snakeStruct.Add(new Vector2(i * cw + 15, 18));
             }
         }
+
+        #endregion
     }
 }
